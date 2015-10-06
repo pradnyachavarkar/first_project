5a864110cca93cf735e663d71a7d2bf9b53b7507
pradnya.c <pradnya.c@riva.co>
2015-10-06 15:17:38 +0530

    added filters

--- tweetlist.js
+++ tweetlist.js
@@ -1,43 +1,45 @@
-var xmlhttp = new XMLHttpRequest();
-// var xmlhttp = new createCORSRequest("GET", 'http://localhost:8080', true);
-var searchedUsers = [];
 
-function loadTweets(){
-	var author = document.getElementById('search_box').value;
+function tweetList(){
+	var tweets = {};
 
-	searchedUsers += author;
-	
-	xmlhttp.open("GET", 'http://localhost:8080?author='+author, true);
-
-	xmlhttp.onreadystatechange = function() {
-	    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-	        var result = xmlhttp.responseText;
-	        console.log(result);
-	        if(result.search("Not authorized") != -1 || result.length <= 2){
-	        	alert("Not authorized to fetch this user's tweets");
-	        }
-	        else{
-		        display(JSON.parse(result), author);
-		    }
-	    }
+	this.addTweet = function(tweet){
+		tweet.detectTags();
+		if(!(tweet.author in tweets)){
+			tweets[tweet.author] = [];
+		}
+		tweets[tweet.author].push(tweet);
 	}
-	
-	xmlhttp.send();
 
-	function display(arr, user) {
-		var ul = document.createElement('ul');
-		ul.setAttribute('id', 'user_' + user);
-		document.getElementById('tweetlist').appendChild(ul);
+	this.print = function(){
+		console.log(tweets);
+	}
 
-	    var out = "";
-	    for(var i = 0; i < arr.length; i++) {
-		    // out += arr[i]['user']['name'] + ":  " + arr[i]['text'] + "<br/><br/>";
-	        var li = document.createElement('li');
-            li.setAttribute('class','tweet');
-            ul.appendChild(li);
-            // t = document.createTextNode(element);
-            li.innerHTML =  arr[i]['user']['name'] + ":  " + arr[i]['text'];
-	    }
-	   // document.getElementById('tweetlist').innerHTML  = out;
+	this.getAllUsers = function(){
+		var users = [];
+		for(val in tweets){
+			users.push(val);
+		}
+		return users;
+	}
+
+	this.getAllTags = function(){
+		var tags = [];
+		for(user in tweets){
+			var userTweets = tweets[user];
+			for(var i = 0; i < userTweets.length; i++){
+				var userTags = userTweets[i].hashtags;
+				for(var j = 0; j < userTags.length; j++){
+					if(tags.indexOf(userTags[j]) == -1)
+					{
+						tags.push(userTags[j]);
+					}
+				}
+			}
+		}
+		return tags;	
+	}
+
+	this.getTweetsByUser = function(user){
+		return tweets[user];
 	}
 }
